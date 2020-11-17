3bf0bf44c0d6183cf5beb334e41f1bfba4b876f2
Feral-Jas <906028518@qq.com>
2020-11-17 15:04:49 +0800

    solve initializer Exception try thread.sleep

--- src/main/java/operator/BatchSourceFunction.java
+++ src/main/java/operator/BatchSourceFunction.java
@@ -33,41 +33,73 @@ public class BatchSourceFunction implements ParallelSourceFunction<String> {
             "jdbc:dm://10.15.0.173:5236/CSSBASE",
             "CSSBASE",
             "1234567890");
-        ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
-        service.scheduleAtFixedRate(
-            () -> {
-                    if (isRunning){
-                        synchronized (sourceContext.getCheckpointLock()){
-                            System.out.println("begin sql...");
-                            PreparedStatement ps = null;
-                            try {
-                                ps = connection.prepareStatement("select * from CSSBASE_CL.S_LOG t where t.TIMESTAMP >=? and t.TIMESTAMP <=?");
-                                LocalDateTime now = LocalDateTime.now();
-                                ps.setTimestamp(1,Timestamp.valueOf(now.minusSeconds(1)));
-                                ps.setTimestamp(2,Timestamp.valueOf(now));
-                                ResultSet resultSet = ps.executeQuery();
-                                while(resultSet.next()){
-                                    String uuid = resultSet.getString(1);
-                                    StringBuilder sb = new StringBuilder();
-                                    sb.append(uuid);
-                                    sb.append(",");
-                                    sb.append(resultSet.getString(2));
-                                    sb.append(",");
-                                    sb.append(resultSet.getString(3));
-                                    sb.append(",");
-                                    Timestamp resTime = resultSet.getTimestamp(4);
-                                    sb.append(resTime);
-                                    sourceContext.collect(sb.toString());
+        while (true){
+            synchronized (sourceContext.getCheckpointLock()){
+                System.out.println("begin sql...");
+                PreparedStatement ps = null;
+                try {
+                    ps = connection.prepareStatement("select * from CSSBASE_CL.S_LOG t where t.TIMESTAMP >=? and t.TIMESTAMP <=?");
+                    LocalDateTime now = LocalDateTime.now();
+                    ps.setTimestamp(1,Timestamp.valueOf(now.minusSeconds(1)));
+                    ps.setTimestamp(2,Timestamp.valueOf(now));
+                    ResultSet resultSet = ps.executeQuery();
+                    while(resultSet.next()){
+                        String uuid = resultSet.getString(1);
+                        StringBuilder sb = new StringBuilder();
+                        sb.append(uuid);
+                        sb.append(",");
+                        sb.append(resultSet.getString(2));
+                        sb.append(",");
+                        sb.append(resultSet.getString(3));
+                        sb.append(",");
+                        Timestamp resTime = resultSet.getTimestamp(4);
+                        sb.append(resTime);
+                        sourceContext.collect(sb.toString());
 
-                                }
-                                ps.close();
-                            } catch (SQLException e) {
-                                e.printStackTrace();
-                            }
-                        }
                     }
-            },0,2,TimeUnit.SECONDS
-        );
+                    ps.close();
+                    Thread.sleep(2000);
+                } catch (SQLException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+//        ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
+//        service.scheduleAtFixedRate(
+//            () -> {
+//                    if (isRunning){
+//                        synchronized (sourceContext.getCheckpointLock()){
+//                            System.out.println("begin sql...");
+//                            PreparedStatement ps = null;
+//                            try {
+//                                ps = connection.prepareStatement("select * from CSSBASE_CL.S_LOG t where t.TIMESTAMP >=? and t.TIMESTAMP <=?");
+//                                LocalDateTime now = LocalDateTime.now();
+//                                ps.setTimestamp(1,Timestamp.valueOf(now.minusSeconds(1)));
+//                                ps.setTimestamp(2,Timestamp.valueOf(now));
+//                                ResultSet resultSet = ps.executeQuery();
+//                                while(resultSet.next()){
+//                                    String uuid = resultSet.getString(1);
+//                                    StringBuilder sb = new StringBuilder();
+//                                    sb.append(uuid);
+//                                    sb.append(",");
+//                                    sb.append(resultSet.getString(2));
+//                                    sb.append(",");
+//                                    sb.append(resultSet.getString(3));
+//                                    sb.append(",");
+//                                    Timestamp resTime = resultSet.getTimestamp(4);
+//                                    sb.append(resTime);
+//                                    sourceContext.collect(sb.toString());
+//
+//                                }
+//                                ps.close();
+//                            } catch (SQLException e) {
+//                                e.printStackTrace();
+//                            }
+//                        }
+//                    }
+//            },0,2,TimeUnit.SECONDS
+//        );
     }
 
     @Override
