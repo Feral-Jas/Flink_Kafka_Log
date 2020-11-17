18ddd1bcf196126e171dca4d4460f773a83f87b6
Feral-Jas <906028518@qq.com>
2020-11-17 15:30:18 +0800

    check if enum singleton can work

--- src/main/java/operator/BatchSourceFunction.java
+++ src/main/java/operator/BatchSourceFunction.java
@@ -18,21 +18,25 @@ public class BatchSourceFunction implements ParallelSourceFunction<String> {
     private volatile boolean isRunning = true;
     @Override
     public void run(SourceContext<String> sourceContext) throws SQLException {
-//        DmJdbc dmInstance = DmJdbc.INSTANCE;
-//        Connection connection1 = dmInstance.getConnection();
-        //Properties dmProp = PropsHelper.getProp("dm.properties");
-        try {
-            Class.forName("dm.jdbc.driver.DmDriver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-//        String DmUrl = dmProp.getProperty("url");
+        DmJdbc dmInstance = DmJdbc.INSTANCE;
+        Connection connection = dmInstance.getConnection();
+//        Properties dmProp = PropsHelper.getProp("dm.properties");
+//        try {
+//            Class.forName("dm.jdbc.driver.DmDriver");
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        String dmUrl = dmProp.getProperty("url");
 //        String username = dmProp.getProperty("username");
 //        String password = dmProp.getProperty("password");
-        Connection connection = DriverManager.getConnection(
-            "jdbc:dm://10.15.0.173:5236/CSSBASE",
-            "CSSBASE",
-            "1234567890");
+//        Connection connection = DriverManager.getConnection(
+//            dmUrl,
+//            username,
+//            password);
+//        Connection connection = DriverManager.getConnection(
+//            "jdbc:dm://10.15.0.173:5236/CSSBASE",
+//            "CSSBASE",
+//            "1234567890");
         while (true){
             synchronized (sourceContext.getCheckpointLock()){
                 System.out.println("begin sql...");
