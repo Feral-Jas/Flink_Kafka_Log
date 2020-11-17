3c2c7b4b68775c8362766c64234d06790fc623c6
Feral-Jas <906028518@qq.com>
2020-11-17 15:00:18 +0800

    solve initializer Exception 1

--- src/main/java/operator/BatchSourceFunction.java
+++ src/main/java/operator/BatchSourceFunction.java
@@ -20,19 +20,19 @@ public class BatchSourceFunction implements ParallelSourceFunction<String> {
     public void run(SourceContext<String> sourceContext) throws SQLException {
 //        DmJdbc dmInstance = DmJdbc.INSTANCE;
 //        Connection connection1 = dmInstance.getConnection();
-        Properties dmProp = PropsHelper.getProp("dm.properties");
+        //Properties dmProp = PropsHelper.getProp("dm.properties");
         try {
             Class.forName("dm.jdbc.driver.DmDriver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        String DmUrl = dmProp.getProperty("url");
-        String username = dmProp.getProperty("username");
-        String password = dmProp.getProperty("password");
+//        String DmUrl = dmProp.getProperty("url");
+//        String username = dmProp.getProperty("username");
+//        String password = dmProp.getProperty("password");
         Connection connection = DriverManager.getConnection(
-            DmUrl,
-            username,
-            password);
+            "jdbc:dm://10.15.0.173:5236/CSSBASE",
+            "CSSBASE",
+            "1234567890");
         ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
         service.scheduleAtFixedRate(
             () -> {
